47cf26819b4c0025c0b43bebddbc2f7eb654270e
Thongqi <141812298+Thongqi@users.noreply.github.com>
2026-05-04 14:56:20 +0800

    to deploy to netlify

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react";
+import { reactRouter } from "@react-router/dev/vite";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactRouter(), netlifyPlugin()],
   server: {
     proxy: {
       "/api": {
