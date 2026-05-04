6139ce94ca92b80e2f6cec8cea12b7954be3787e
Thongqi <141812298+Thongqi@users.noreply.github.com>
2026-05-04 15:28:18 +0800

    remove netlify setting

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), netlifyPlugin()],
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
