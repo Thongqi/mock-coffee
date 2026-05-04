14e74d4bc1df4fdf747a1d59d7b3d232a3f9ecc8
Thongqi <141812298+Thongqi@users.noreply.github.com>
2026-05-04 15:06:16 +0800

    add in redirects files for netlifty

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-import { reactRouter } from "@react-router/dev/vite";
+import react from "@vitejs/plugin-react";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [reactRouter(), netlifyPlugin()],
+  plugins: [react(), netlifyPlugin()],
   server: {
     proxy: {
       "/api": {
